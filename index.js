51997d4372f441daf9cc0ee3f399b33bba20154c
Aamir Khan <63204104+HoodieB@users.noreply.github.com>
2022-10-29 21:05:37 +0100

    Fixed .ENV api key

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const NewsApi = require('./newsApi')
-const ArticleModel = require('./articleModel')
-
+const FeedModel = require('./feedModel')
+const NewsView = require('./newsView')
 
 const api = new NewsApi
+const feed = new FeedModel
+const view = new NewsView(feed)
+
+// const envFilePlugin = require('esbuild-envfile-plugin');
+
+// require('esbuild').build({
+//     plugins: [envFilePlugin]
+// });
+
+api.loadSummaries((response) => {
+  feed.setArticles(response)
+})
+
+console.log("I've displayed the articles!")
